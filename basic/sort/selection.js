fd4dad06d40d3f5fc6e38b1f59359bba81c83ed0
ajmalmirsha <ajmalmirshaa2004@gmail.com>
2024-06-08 06:28:51 +0530

    dsa(singly linked list): revising

---
+++ basic/sort/selection.js
@@ -0,0 +1,121 @@
+
+
+
+// function selectionSort (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1; j<arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+// }
+
+// const arr = [3,5,5,3,5,53,5]
+
+// selectionSort(arr)
+
+// console.log(arr);
+
+
+//  function selection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1; j<arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+//  }
+
+//  const arr = [45,34,345,2,2,23,243,5,32]
+
+//  selection(arr)
+
+//  console.log(arr);
+
+
+
+
+// function slection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//        for(let j=i+1; j<arr.length; j++){
+//         if(arr[i] > arr[j]){
+//            [arr[i],arr[j]] = [arr[j],arr[i]]
+//         }
+//        }
+//    }
+// }
+
+// const arr = [5,4,345,345,34,3]
+
+// slection(arr)
+
+// console.log(arr);
+
+
+// function selection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1; j < arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+// }
+
+// const arr = [4,34,3,3,4,534,5,345]
+
+// selection(arr)
+
+// console.log(arr);
+
+
+// function selection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1; j<arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+// }
+
+// const arr = [234,23,3,53,45,345]
+
+// selection(arr)
+
+
+// console.log(arr);
+
+
+// function selection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1;j<arr.length;j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+// }
+
+// const arr = [324,234,2,34,23,55]
+
+// selection(arr)
+
+// console.log(arr);
+
+// function selection (arr) {
+//     for(let i=0; i<arr.length; i++){
+//         for(let j=i+1; j<arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 [arr[i],arr[j]] = [arr[j],arr[i]]
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// const arr  = [23,23,43,434,534,24,24,24,2435435]
+
+// console.log(selection(arr));
