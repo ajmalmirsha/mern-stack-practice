d4ff9f19126f765ef3c7fd4e85fdb8d8e87a2058
ajmalmirsha <ajmalmirshaa2004@gmail.com>
2024-07-09 19:55:31 +0530

    work out problems

--- basic/sort/sort.js
+++ basic/sort/sort.js
@@ -23,8 +23,23 @@ function selectionSort(arr) {
   }
 }
 
+function quick(arr) {
+  if (arr.length <= 1) return arr;
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quick(left), pivot, ...quick(right)]
+}
+
 const arr = [2, 4, -3, 53, 3.4];
 
-selectionSort(arr);
+quick(arr);
 
 console.log(arr);
